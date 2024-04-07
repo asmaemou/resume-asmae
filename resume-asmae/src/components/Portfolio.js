52c24b16301aa8cd968997150d4a7b277b6a15e3
asmaemou <amouradi2002@gmail.com>
2024-04-07 17:22:55 +0000

    changing my services

--- resume-asmae/src/components/Portfolio.js
+++ resume-asmae/src/components/Portfolio.js
@@ -1,27 +1,26 @@
-import taxi from "../images/Best-Taxi-Apps.jpg";
-import bookStore from "../images/book-store.jpg";
+import defyfit from "../images/defyfit.png";
+import ecommerce from "../images/mobile-app.png";
 import portfolioHome from "../images/portfolio-home.jpg";
-
 const Portfolio = () => {
   return (
     <>
       <div id="portfolio">
         <div className="container">
-          <h1 className="sub-title">My Projects</h1>
+          <h1 className="sub-title">What I have acheived?</h1>
           <div className="work-list">
             <div className="work">
-              <img src={taxi} alt="The app taxi" />
+              <img src={defyfit} alt="Mobile App Defy Fit" />
               <div className="layer">
-                <h4 className="work-title">Taxiji</h4>
+                <h4 className="work-title">Defy Fit</h4>
                 <p className="work-description">
-                  This application aimed to help college student to save time by
-                  reserving a taxi at a specific time and a specific number of
-                  people.
+                  A mobile fitness app that allows users to create and share their own workout routines, track progress, and connect  
+                  with other trainers to work against each other.  
                 </p>
-                <h5 className="titleS">Built with</h5>
+                <h5 className="titleS">Technology used:</h5>
                 <ul className="work-tools">
                   <li>React</li>
                   <li>JavaScript</li>
+                  <li>Spring Boot</li>
                 </ul>
                 <a href="/#">
                   <i className="fa-solid fa-up-right-from-square"></i>
@@ -29,12 +28,12 @@ const Portfolio = () => {
               </div>
             </div>
             <div className="work">
-              <img src={bookStore} alt="Book store Website" />
+              <img src={ecommerce} alt="Mobile Application" />
               <div className="layer">
-                <h3>Book Store</h3>
+                <h3>E-commerce mobile application</h3>
                 <p>
-                  The website aimed to help the librarian in the organization
-                  process and it helps the user to find the book easily.
+                  This is an E-commerce mobile application that can be installed in Android and IOS 
+                  devices, it was developed using React Native for the frontend and Django for the backend.
                 </p>
                 <a href="/#">
                   <i className="fa-solid fa-up-right-from-square"></i>
